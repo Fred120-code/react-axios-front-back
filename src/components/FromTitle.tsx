75e1a1382be5813af731a5a92715181a73727351
Fred120-code <fredayemtsa@gmail.com>
2025-08-10 22:17:07 +0100

    ajout de la fonction de det de la force du mot de passe

--- src/components/FromTitle.tsx
+++ src/components/FromTitle.tsx
@@ -6,8 +6,8 @@ interface TitleProps{
 }
 const FromTitle = ({title, comment, style}:TitleProps) => {
   return (
-    <div className={cn("gap-2 flex flex-col in-checked: xl:text-center", style)}>
-        <h1 className='text-[2rem] lg:text-[3rem] w-full font-semibold text-center'>
+    <div className={cn("flex flex-col in-checked: xl:text-center", style)}>
+        <h1 className='text-[1.8rem] lg:text-[3rem] w-full font-semibold text-center'>
             {title}
         </h1>
         <h6 className="text-[1rem] md:text-[1.2rem] font-normal text-gray-600 text-center">
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -2,8 +2,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import logo2 from '../../assets/logo2.svg'
 import FromTitle from "../../components/FromTitle"
-import Label from "../../components/ui/Label"
-import Input from "../../components/ui/Input"
+import {Label} from "../../components/ui/Label"
+import {Input} from "../../components/ui/Input"
 
 
 // fonction permettant de calculer la force du mots de passe
@@ -24,37 +24,91 @@ const getPasswordStrength = (password:string)=>{
   return Math.min(score, 5) // retoune un nombre qui est inferieur à 5
 }
 
-const AuthRegister = ()=>{
-  const navigate = useNavigate()
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [name, setName] = useState("")
-
-  //fonction handleSubmit
-
-}
-
-
 const Register = () => {
+
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    //API call
+    navigate(`/auth/verify-otp/${email}`);
+  };
+
+  const strength = getPasswordStrength(password);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-10">
+    <div className="flex flex-col items-center m-20 gap-4 h-full px-10 w-full">
       <div>
           <img src={logo2} alt="logo2" />
       </div>
-      <div className="w-full flex items-center flex-col max-w-lg space-y-4">
+      <div className="w-full flex items-center flex-col max-w-lg p-1">
         <FromTitle 
           title="Creation de compte" 
           comment="Rentrez vos infos pour creer votre compte." 
           style="w-full"/>
       </div>
-      <form action="" className="space-y-4">
+      <form action="" onSubmit={handleSubmit} className="space-y-1 w-1/2">
+        <div className="">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name"
+                    type="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Your name"
+                    className="w-full focus:right-0 focus:border-blue-700 transition-all"
+            />
+        </div>
         <div>
-            <Label >Email</Label>
-            <Input/>
+            <Label htmlFor="email">Email</Label>
+            <Input id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="abc@example.com"
+                    className="w-full focus:right-0 focus:border-blue-700 transition-all"
+            />
+        </div>
+        <div>
+            <Label htmlFor="password">Password</Label>
+            <Input id="password"
+                    type="password"
+                    value={password}
+                    placeholder="****"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full focus:right-0 focus:border-blue-700 transition-all"
+            />
+        </div>
+        <div className="mt-2 flex items-center space-x-1 w-full">
+          {
+            [1,2,3,4,5].map((segment,index)=>(
+              <div
+                key={index}
+                className={`w-full h-1 ${
+                  index <strength 
+                      ? index === 0
+                        ?"bg-red-500"
+                        :index === 1
+                        ?"bg-orange-500"
+                        :index === 2
+                        ?"bg-yellow-500"
+                        :index === 3
+                        ? "bg-green-500"
+                        :"bg-blue-500"
+                      :"bg-gray-300"
+                } rounded-b-full transition-colors duration-300`} >
+              </div>
+            ))
+          }
         </div>
       </form>
     </div>
   )
 }
+
 
 export default Register
